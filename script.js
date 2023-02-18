a8d37d9989e85d8b26984e45b9cf2cbfcb30280f
Selda Erkilic <seldaerkilic@gmail.com>
2023-02-18 17:22:52 -0500

    added style to hamburger menu

--- script.js
+++ script.js
@@ -2,9 +2,11 @@
 
 const menu = document.querySelector('.menu');
 const nav = document.querySelector('.navBar');
+const menuBackground = document.querySelector('.menuBackground');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('open')
     nav.classList.toggle('navList')
+    menuBackground.classList.toggle('greenShade')
 })
 
