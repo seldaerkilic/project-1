3086e713efc8ce0417d48bb6c15d7c183177f0f9
Selda Erkilic <seldaerkilic@gmail.com>
2023-02-18 16:44:21 -0500

    added hamburger menu animation

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 // Create hamburger menu
 
+const menu = document.querySelector('.menu');
+const nav = document.querySelector('.navBar');
+
+menu.addEventListener('click', () => {
+    menu.classList.toggle('open')
+    nav.classList.toggle('navList')
+})
+
